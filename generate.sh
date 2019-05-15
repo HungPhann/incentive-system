7f56ec46e2790e00abb8b86de894d9e0cfa63337
hungphann <phanhuyhung1707@gmail.com>
2019-05-15 10:30:42 +0200

    First commit

---
+++ generate.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+export PATH=$GOPATH/src/github.com/hyperledger/fabric/build/bin:${PWD}/bin:${PWD}:$PATH
+export FABRIC_CFG_PATH=${PWD}
+CHANNEL_NAME=mychannel
+
+# remove previous crypto material and config transactions
+rm -fr config/*
+rm -fr crypto-config/*
+
+# generate crypto material
+./bin/cryptogen generate --config=./crypto-config.yaml
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate crypto material..."
+  exit 1
+fi
+
+# generate genesis block for orderer
+./bin/configtxgen -profile ThreeOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate orderer genesis block..."
+  exit 1
+fi
+
+# generate channel configuration transaction
+./bin/configtxgen -profile MyChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate channel configuration transaction..."
+  exit 1
+fi
+
+# generate anchor peer transaction for org1
+./bin/configtxgen -profile MyChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate anchor peer update for Org1MSP..."
+  exit 1
+fi
+
+
+# generate anchor peer transaction for org2
+./bin/configtxgen -profile MyChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate anchor peer update for Org1MSP..."
+  exit 1
+fi
