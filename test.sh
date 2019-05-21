84aeb2b455ea3c545f6bb22fbb8370eb1564ffa1
hungphann <phanhuyhung1707@gmail.com>
2019-05-21 12:20:58 +0200

    Add test.sh

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+prefix="{\"Args\":[\"issueToken\","
+suffix=",\"10\"]}"
+quote="\""
+
+for i in {1..100}
+do
+  arg=$prefix$quote$i$quote$suffix
+  docker exec -e "CHANNEL_NAME=mychannel" cli.org1.example.com peer chaincode invoke -o orderer.example.com:7050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C $CHANNEL_NAME -n mycc --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt --peerAddresses peer0.org2.example.com:8051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt -n contract_0 -C mychannel -c $(echo $arg)
+done
